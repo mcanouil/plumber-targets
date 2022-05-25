245e0e021bf80051b44730a91fe323a879e00804
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-05-25 21:45:57 +0200

    feat: add target for data

--- _targets.R
+++ _targets.R
@@ -2,7 +2,11 @@ library(targets)
 
 list(
   tar_target(
+    name = data,
+    command = data.frame(x = rnorm(100), y = rnorm(100))
+  ),
+  tar_target(
     name = model,
-    command = lm(y ~ x, data = data.frame(x = rnorm(100), y = rnorm(100)))
+    command = lm(y ~ x, data = data)
   )
 )
